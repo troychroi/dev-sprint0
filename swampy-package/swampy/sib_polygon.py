e940bf158b5409ab5f4b42931adc1c728e94e153
Troy Church <tchurch@berklee.edu>
2013-06-02 17:02:00 -0400

    changed some integers to float and things worked better

--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -1,34 +1,38 @@
 # Polygon excercise from Week 0
 
 # Name:
-
+import math
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
-bob = Turtle()				
-bob.delay = 0.01
+bob = Turtle()	
 ray = Turtle()
-jay = Turtle()
+jay = Turtle()			
+bob.delay = 0.01
+ray.delay = 0.01
+jay.delay = 0.01
+
 
 # This is where you put code to move bob
 
 def polygon(turtle, l, n):
 		for i in range(n):
 			fd(turtle, l)
-			rt(turtle, 360/n)
+			rt(turtle, 360.00/n)
 polygon(ray, 50, 8)
  
 def circle(turtle, l, n):
 	for i in range(n):
 		fd(turtle, l/n)
-		rt(turtle, 360/n)
-circle(jay, 200, 100)
+		rt(turtle, 360.00/n)
+circle(jay, 600, 300)
+
 
 def arc(turtle, l, n, theta):
 	for i in range(n*theta/360):
 		fd(turtle, l/n)
-		rt(turtle, 360/n)
-arc(bob, 100, 50, 90)
+		rt(turtle, 360.00/n)
+arc(bob, 100, 50, 180)
 
 
 
